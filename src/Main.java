900e7afbdc3784aa8a01b51f8c826d789748dd48
rusnakma@ukr.net <rusnakma@ukr.net>
2023-04-06 09:29:43 +0300

    v2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,78 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(" Lab 3");
 	// write your code here
+        Task1();
+        Task2();
+        Task3();
+    }
+
+    private static void Task1() {
+        System.out.println(" Task 1");
+        Trial array[] = new Trial[4];
+
+        array[0] = new Trial(10);
+        array[1] = new Test(10, 10);
+        array[2] = new Exam(10, 10, 2);
+        array[3] = new FinalExam(10, 10);
+
+        for (var item: array) {
+            item.passingTheTest(4);
+        }
+
+        for (var item: array) {
+            System.out.println(item.show() + "\n");
+        }
+    }
+
+    private static void Task2() {
+        System.out.println(" Task 2");
+        int []array = {1 ,3 ,5 ,2 ,3, 2};
+        Container []containerArray = new Container[2];
+
+        containerArray[0] = new Choice(array);
+        containerArray[1] = new Bubble(array);
+
+        System.out.println(containerArray[0].equals(containerArray[1]));
+
+        containerArray[0].sort();
+        containerArray[1].sort();
+
+        System.out.println(containerArray[0].equals(containerArray[1]));
+
+        System.out.println(containerArray[0].norma());
+        System.out.println(containerArray[1].norma());
+
+        System.out.println(containerArray[0].toString());
+        System.out.println(containerArray[1].toString());
+    }
+
+    private static void Task3() {
+        System.out.println("\n Task 3");
+        int []array = {1 ,3 ,5 ,2 ,3, 2};
+        Container2 []containerArray2 = new Container2[3];
+
+        containerArray2[0] = new Choice2(array);
+        containerArray2[1] = new Choice2(array);
+        containerArray2[2] = new Bubble2(array);
+
+        System.out.println(containerArray2[0].equals(containerArray2[1]));
+
+        containerArray2[0].sort();
+        containerArray2[2].sort();
+
+        System.out.println(containerArray2[0].equals(containerArray2[1]));
+        System.out.println(containerArray2[0].equals(containerArray2[2]));
+
+        System.out.println(containerArray2[0].norma());
+        System.out.println(containerArray2[2].norma());
+
+        System.out.println(containerArray2[0].toString());
+        System.out.println(containerArray2[2].toString());
     }
 }
+
+
+
+
+
+
--- src/Task1.java
+++ src/Task1.java
@@ -1,2 +1,129 @@
-package PACKAGE_NAME;public class Task1 {
+class Trial {
+    private int score;
+    private int maxScore;
+
+    private boolean validity;
+
+    public Trial(int maxScore) {
+        this.maxScore = maxScore;
+        validity = false;
+    }
+
+    protected int getMaxScore() {
+        return maxScore;
+    }
+    protected int getScore() {
+        return score;
+    }
+    protected boolean getValidity() {
+        return validity;
+    }
+    public void passingTheTest(int score) {
+        if (score > this.maxScore) {
+            validity = false;
+            return;
+        }
+        validity = true;
+        this.score = score;
+    }
+
+    public String show() {
+        if (validity == false) return "test not started";
+        return "The test is written for " + score + " out of " + maxScore;
+    }
+}
+
+
+class Test extends Trial {
+    private int numberOfTests;
+    private int numberOfCorrectAnswers;
+
+    public Test(int maxScore, int numberOfTests) {
+        super(maxScore);
+        this.numberOfTests = numberOfTests;
+    }
+
+    public void passingTheTest(int numberOfCorrectAnswers) {
+        this.numberOfCorrectAnswers = numberOfCorrectAnswers;
+        super.passingTheTest(numberOfCorrectAnswers * getMaxScore() / numberOfTests);
+    }
+
+    public String show() {
+        if (getValidity() == false) return "test not started";
+        return super.show() + "\n" + numberOfCorrectAnswers + " out of " + numberOfTests + " correct answers ";
+    }
+}
+
+
+class Exam extends Test {
+    private int passingScore;
+    private String evaluation;
+
+    public Exam(int maxScore, int numberOfTests, int passingScore) {
+        super(maxScore, numberOfTests);
+        this.passingScore = passingScore;
+    }
+    private void setEvaluation() {
+        if (getScore() < passingScore) {
+            evaluation = "test failed";
+        }
+        else {
+            evaluation = "test is passed";
+        }
+    }
+
+
+    public void passingTheTest(int numberOfCorrectAnswers) {
+        super.passingTheTest(numberOfCorrectAnswers);
+        setEvaluation();
+    }
+
+    public String show() {
+        if (getValidity() == false) return "test not started";
+        return super.show() + "\n" + evaluation;
+    }
+
+}
+
+
+class FinalExam extends Exam {
+    private String stringRating;
+
+    public FinalExam(int maxScore, int numberOfTests) {
+        super(maxScore, numberOfTests, maxScore / 2);
+    }
+
+    private void setStringRating() {
+        if (getScore() > 90 * getMaxScore() / 100)
+        {
+            stringRating = "A";
+        }
+        else if (getScore() > 80 * getMaxScore() / 100)
+        {
+            stringRating = "B";
+        }
+        else if (getScore() > 70 * getMaxScore() / 100)
+        {
+            stringRating = "C";
+        }
+        else if (getScore() > 50 * getMaxScore() / 100)
+        {
+            stringRating = "D";
+        }
+        else
+        {
+            stringRating = "E";
+        }
+    }
+
+    public void passingTheTest(int numberOfCorrectAnswers) {
+        super.passingTheTest(numberOfCorrectAnswers);
+        setStringRating();
+    }
+
+    public String show() {
+        if (getValidity() == false) return "test not started";
+        return super.show() + "\n" + "string rating: " + stringRating;
+    }
+
 }
